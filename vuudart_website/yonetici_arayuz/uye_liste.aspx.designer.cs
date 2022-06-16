200e25d6f52e9a8c9f4d32392573bd5f480376a7
salihakdogan <salihakdogan1@gmail.com>
2022-06-16 23:59:22 +0300

    tema ayarlari ve uye listedeki bildirim kismi kaldirildi

--- vuudart_website/yonetici_arayuz/uye_liste.aspx.designer.cs
+++ vuudart_website/yonetici_arayuz/uye_liste.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace vuudart_website.yonetici_arayuz
 
     public partial class uye_liste
     {
-
-        /// <summary>
-        /// kayitbasarili control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl kayitbasarili;
     }
 }
