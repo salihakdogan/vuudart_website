4d477de84086663feee1933938f303e3ce2daefe
salihakdogan <salihakdogan1@gmail.com>
2022-06-23 19:19:29 +0300

    kategori ve hammadde eklemek icin droplist classlari olusturuldu droplistlere veri cekmek ya da eklemek icin metotlar yazildi

---
+++ vuudart_website/cs/Droplist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vuudart_website.cs
+{
+    public class Droplist
+    {
+        string kategoriad, kategoriaciklama, hammaddead, hammaddeaciklama;
+
+        public string Kategoriad { get => kategoriad; set => kategoriad = value; }
+        public string Kategoriaciklama { get => kategoriaciklama; set => kategoriaciklama = value; }
+        public string Hammaddead { get => hammaddead; set => hammaddead = value; }
+        public string Hammaddeaciklama { get => hammaddeaciklama; set => hammaddeaciklama = value; }
+    }
+}
---
+++ vuudart_website/cs/DroplistCRUD.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace vuudart_website.cs
+{
+    public class DroplistCRUD
+    {
+        Db db = new Db();
+
+        public bool ykategoriekle(Droplist yenikategori)
+        {
+            bool sonuc = true;
+            db.ac();
+
+            SqlCommand komut = new SqlCommand("insert into Kategoriler values (@1,@2)", db.baglanti);
+
+            komut.Parameters.AddWithValue("@1", yenikategori.Kategoriad);
+            komut.Parameters.AddWithValue("@2", yenikategori.Kategoriaciklama);
+
+            int durum = Convert.ToInt16(komut.ExecuteNonQuery());
+
+            if (durum == 0)
+            {
+                sonuc = false;
+            }
+
+            db.kapat();
+
+            return sonuc;
+        }
+
+        public DataTable kategoriliste()
+        {
+            DataTable dt = new DataTable();
+
+            db.ac();
+            SqlCommand komut1 = new SqlCommand("select distinct Ad from Kategoriler", db.baglanti);
+            SqlDataAdapter adp1 = new SqlDataAdapter(komut1);
+            adp1.Fill(dt);
+            db.kapat();
+            return dt;
+        }
+
+        public bool yhammaddeekle(Droplist yenihammadde)
+        {
+            bool sonuc = true;
+            db.ac();
+
+            SqlCommand komut = new SqlCommand("insert into Hammaddeler values (@1,@2)", db.baglanti);
+
+            komut.Parameters.AddWithValue("@1", yenihammadde.Hammaddead);
+            komut.Parameters.AddWithValue("@2", yenihammadde.Hammaddeaciklama);
+
+            int durum = Convert.ToInt16(komut.ExecuteNonQuery());
+
+            if (durum == 0)
+            {
+                sonuc = false;
+            }
+
+            db.kapat();
+
+            return sonuc;
+        }
+
+        public DataTable hammaddeliste()
+        {
+            DataTable dt = new DataTable();
+
+            db.ac();
+            SqlCommand komut1 = new SqlCommand("select distinct Ad from Hammaddeler", db.baglanti);
+            SqlDataAdapter adp1 = new SqlDataAdapter(komut1);
+            adp1.Fill(dt);
+            db.kapat();
+            return dt;
+        }
+
+    }
+}
