efe7f39f7d111946e1a81a8453fb151a0d5c0939
salihakdogan <salihakdogan1@gmail.com>
2022-07-04 11:34:10 +0300

    session ile giris yapan kisinin sepetinde kac tane urun oldugunu gosteren metot yazildi

--- vuudart_website/cs/SepetCRUD.cs
+++ vuudart_website/cs/SepetCRUD.cs
@@ -85,6 +85,19 @@ namespace vuudart_website.cs
             return cesitadet;
         }
 
+        public int sepettoplamadeturun(string pmail)
+        {
+            int cesitadet;
+
+            db.ac();
+            SqlCommand komut = new SqlCommand("select sum(UrunAdet) from Sepet where Durum=0 and UyeMail=@a", db.baglanti);
+            komut.Parameters.AddWithValue("@a", pmail);
+            cesitadet = Convert.ToInt16(komut.ExecuteScalar());
+
+            db.kapat();
+            return cesitadet;
+        }
+
         public DataTable sepetfiyattoplami(string pmail)
         {
             DataTable dt = new DataTable();
